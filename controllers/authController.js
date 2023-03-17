cb469f4c5a2f6c44978521aaad00cf6f1d649c98
Lovish Kamboj <Coc2610199914@gmail.com>
2023-03-17 13:51:48 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,7 +9,7 @@ const cookieOptions = {
     Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
   ),
   httpOnly: true,
-  secure: req.secure || req.headers("x-forwarded-proto") === "https",
+  secure: req.secure || req.headers["x-forwarded-proto"] === "https",
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
